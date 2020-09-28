8f68ac88de80e44a2462ef7bd5f2e9d61f1eb677
Zdenek Bidrman <31826605+Bidrman@users.noreply.github.com>
2020-09-28 18:21:40 +0200

    code review

--- src/features/whyInvest/Invest.jsx
+++ src/features/whyInvest/Invest.jsx
@@ -11,9 +11,11 @@ const Invest = ({ heading }) => {
             </Row>
             <Row>
                 <Col xs='12' sm='4'>
-                    <Card className=''>
-                        <HouseDoor height={90} width={90} fill='#9c3' className='card__icon' />
-                        <h5 className='car-title text-center mb-4'>bubub</h5>
+                    <Card className='custom-card'>
+                        <HouseDoor height={90} width={90} fill='#9c3' className='custom-card__icon mb-3' />
+                        <h5 className='car-title text-center mb-4'>
+                            Zhodnocení peněz ležících na účtě ladem
+                        </h5>
                         <p className='card-text text-justify'>
                             Investování do podílových fondů je v dnešní dobe jeden ze způsobů, jak můžete
                             zhodnotit své volné peníze, které by jinak ležely ladem na běžném účtě.
